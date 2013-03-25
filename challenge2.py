882ea5161db3aecf51181e12160be90240c53307
jasonqracker <jason.quiroga@rackspace.com>
2013-03-25 14:13:01 -0500

    modified:   challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -1,7 +1,7 @@
 import pyrax
 import time
 
-pyrax.set_http_debug(True)
+#pyrax.set_http_debug(True)
 pyrax.set_credential_file("/home/hal/.rackspace_cloud_credentials.txt")
 
 cs = pyrax.cloudservers
@@ -11,20 +11,21 @@ servers = cs.servers.list()
 servDict = {}
 
 print "Choose a server to clone from the list"
+print ""
 
 for num, server in enumerate(servers):
   print ("%s " + " Name: " + "%s" + "  ID: " + "%s") % (num, server.name, server.id)
   servDict[str(num)] = server.id
 
-choice = raw_input("Server number: ")
+choice = raw_input("Select server number: ")
 while choice not in servDict:
   if choice is not None:
     print "Selection not valid, please choose again "
     choice = raw_input("Select server number: ")
-#selection = choice 
+
 server_id = servDict[str(choice)]
 
-imageName = raw_input("Name the image: ")
+imageName = raw_input("Name the image to be created: ")
 
 flavors = cs.flavors.list()
 
@@ -33,24 +34,31 @@ flavDict = {}
 print "Choose a flavor for the new server: "
 
 for num, flavor in enumerate(flavors):
-  print "%s %s" % (num, flavor)
-  flavDict[str(num)] = flavor.id
+  print "%s    %s" % (num, flavor)
+  flavDict[str(num)] = flavor
 
-choice = raw_input("Select flavor: ")
+choice = raw_input("Select flavor number: ")
 while choice not in flavDict:
   if choice is not None:
     print "Selection not valid, please choose again: "
     choice = raw_input("Select flavor number: ")
 
-flavor_id = flavDict[str(choice)]
+flavor_id = flavDict[choice]
 
 image_id = [cs.servers.create_image(server_id, imageName)][0]
 
 print "Image %s is being created with the ID: %s" % (imageName, image_id)
+print ""
+print "Image saving. Please stand by..."
+print ""
+images = cs.images.list()
+while str(images[0].status) != 'ACTIVE':
+  time.sleep(10)
+  images = cs.images.list()
 
 serverName = raw_input("Name the new cloud server: ")
 
-server = cs.servers.create(serverName, image_id.id, flavor_id)
+server = cs.servers.create(serverName, image_id, flavor_id.id)
 print "ID: ", server.id
 print "Status: ", server.status
 print "Admin Password: ", server.adminPass
