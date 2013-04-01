4a258a07a69d6299dcc0a136688ad89328858c77
Hal <hal@unknownb88d122e8312.gateway.2wire.net>
2013-04-01 17:03:32 -0500

    challenge6

---
+++ challenge6.py
@@ -0,0 +1,17 @@
+# challenge6 Write a script
+# that creates a CDN-enabled container in
+# Cloud Files. Worth 1 Point
+
+import pyrax
+
+pyrax.set_credential_file("/Users/hal/.rackspace_cloud_credentials.txt")
+
+cf = pyrax.cloudfiles
+
+print "Let's create a CDN enabled CloudFiles container."
+name = raw_input("Name your container: ")
+
+cont = cf.create_container(name)
+print "Container created: ", cont.name
+
+cf.make_container_public(cont.name, ttl=1800)
