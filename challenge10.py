e22dae2a1692a2fe035bc1f051eb2b78a60ac61c
jasonQ <jason.quiroga@rackspace.com>
2013-04-05 17:28:16 -0500

    updates

--- challenge10.py
+++ challenge10.py
@@ -16,6 +16,7 @@ import os
 pyrax.set_credential_file("/Users/jaso4210/.rackspace_cloud_credentials.txt")
 
 cs = pyrax.cloudservers
+clb = pyrax.cloud_loadbalancers
 
 #find ubuntu 12.04 image:
 
@@ -36,34 +37,58 @@ with open(sshPath, "r") as root_ssh_auth_keys:
 files = {"/root/.ssh/authorized_keys" : content}
 
 #create 2 servers:
+#find ubuntu 12.04 image:
 
-n = 1
-servers = {}
-while n < 3:
-    name = "chal9Srv" + str(n)
-    servers[name]  = cs.servers.create(name, ubuntu124_image.id, flavor_512.id, files=files)
-    print "=" * 40
-    print "Provisioning Server Name: ", servers[name].name
-    print "Server ID:", servers[name].id
-    print "Server Status:", servers[name].status
-    print "Admin password:", servers[name].adminPass
-    print "Waiting for network provisioning..."
-    while not (servers[name].networks):
-        time.sleep(15)
-        servers[name] = cs.servers.get(servers[name].id)
-    print "Public Network Address(es) provisioned:", str(servers[name].networks["public"])
-    print "Private Network Address(es) provisioned: ", str(servers[name].networks["private"])
-    print "'",servers[name].name,"' successfully provisioned."   
-    print "=" * 40
-    n += 1
+ubuntu124_image = [img for img in cs.images.list()
+        if "Ubuntu 12.04" in img.name][0]
+
+#select 512MB flavor:
+
+flavor_512 = [flavor for flavor in cs.flavors.list()
+        if flavor.ram == 512][0]
+
+#create 2 servers - server1 and server2:
+
+server1 = cs.servers.create("server1", ubuntu124_image, flavor_512)
+print "=" * 40
+print "Provisioning Server Name: ", server.name
+print "Server ID:", server1.id
+print "Server Status:", server1.status
+print "Admin password:", server1.adminPass
+print "Waiting for network provisioning..."
+while not (server1.networks):
+    time.sleep(15)
+    server1 = cs.servers.get(server1.id)
+print "Public Network Address(es) provisioned:", str(server1.networks["public"])
+print "Private Network Address(es) provisioned: ", str(server1.networks["private"])
+print "'",server1.name,"' successfully provisioned."   
+print "=" * 40
+print ""
+        
+        
+server2 = cs.servers.create("server2", ubuntu124_image, flavor_512)
+print "=" * 40
+print "Provisioning Server Name: ", server2.name
+print "Server ID:", server2.id
+print "Server Status:", server2.status
+print "Admin password:", server2.adminPass
+print "Waiting for network provisioning..."
+while not (server2.networks):
+    time.sleep(15)
+    server2= cs.servers.get(server2.id)
+print "Public Network Address(es) provisioned:", str(server2.networks["public"])
+print "Private Network Address(es) provisioned: ", str(server2.networks["private"])
+print "'",server2.name,"' successfully provisioned."   
+print "=" * 40
+print ""
 
 # create LB:
 
 print "Creating loadbalancer..."
 
 # Obtain the servers' private IPs: 
-server1_ip = servers['chal9Srv1'].networks["private"][0]
-server2_ip = servers['chal9Srv2'].networks["private"][0]
+server1_ip = server1.networks["private"][0]
+server2_ip = server2.networks["private"][0]
 
 # Create the nodes
 node1 = clb.Node(address=server1_ip, port=80, condition="ENABLED")
@@ -88,3 +113,6 @@ lb.add_health_monitor(type="HTTP", delay=10, timeout=10,
 
 html = "<html><body>Something is afoot at the CircleK!</body></html>"
 lb.set_error_page(html)
+
+print "HTTP monitor and custom error page provisioned:"
+
