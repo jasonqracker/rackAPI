d0e3992aecd05660723943ace7f30610f61deb7e
jasonqracker <jason.quiroga@rackspace.com>
2013-04-26 15:34:14 -0500

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #Challenge 1: Write a script that builds three 512 MB Cloud Servers that following a
 #similar naming convention. (ie., web1, web2, web3) and returns
 #the IP and login credentials for each server. Use any image you want. Worth 1 point
