ee57ec43734cd169cb107ac14177178fc774ecbb
jasonqracker <jason.quiroga@rackspace.com>
2013-04-26 15:58:42 -0500

    Update challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -3,7 +3,7 @@
 import pyrax
 import time
 
-pyrax.set_credential_file("/home/hal/.rackspace_cloud_credentials.txt")
+pyrax.set_credential_file("~/.rackspace_cloud_credentials.txt")
 
 cs = pyrax.cloudservers
 
