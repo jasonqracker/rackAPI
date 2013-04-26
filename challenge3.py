58b61ebe1da8eff65588999b69d6f5ab7dd8b7e3
jasonqracker <jason.quiroga@rackspace.com>
2013-04-26 15:34:45 -0500

    Update challenge3.py

--- challenge3.py
+++ challenge3.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #Challenge 3: Write a script that accepts a directory as an argument as well as a container name.
 #The script should upload the contents of the specified directory to the
 #container (or create it if it doesn't exist). The script should handle errors
